8911dce0f5b8d7e98c66828f12f8e610e0380674
Peter Vliet <petervliet@gmail.com>
2015-07-28 16:11:12 -0600

    configure routes, first spec passes

--- spec/features/counter_spec.rb
+++ spec/features/counter_spec.rb
@@ -5,4 +5,8 @@ describe 'user can see landing page' do
     visit '/'
     expect(page).to have_content('hey, here are numbers')
   end
+  scenario 'user sees integers on page' do
+    visit '/'
+    expect(page).to have_content()
+  end
 end
